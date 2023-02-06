578fadfe2d0cacd82cd75d88b6071acfac81ca8f
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-02-06 20:32:49 +0100

    test: workflow and example

---
+++ example/index.js
@@ -0,0 +1,7 @@
+import 'foo';
+import './foo.js'
+import './foo.json'
+// eslint-disable-next-line require-extensions/require-extensions
+import './foo'
+// eslint-disable-next-line require-extensions/require-extensions
+import './bar.json'
